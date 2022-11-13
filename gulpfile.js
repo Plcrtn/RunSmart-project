bf373fb37a344db48fc4d0a05b63258c7332acd4
Dmitry Lisitsyn <polcertaain@gmail.com>
2022-11-13 18:25:46 +0200

    hui

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,21 @@
-const gulp        = require('gulp');
+const gulp = require('gulp');
 const browserSync = require('browser-sync');
 const sass = require('gulp-sass')(require('sass'));
 const rename = require("gulp-rename");
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
 
-// Static server
 gulp.task('server', function() {
-    browserSync.init({
+    browserSync({
         server: {
             baseDir: "src"
         }
     });
+    gulp.watch("src/*.html").on("change", browserSync.reload);
 });
 
 gulp.task('styles', function() {
-    return gulp.src("src/sass/*.+(scss|sass)")
+    return gulp.src("src/sass/**/*.+(scss|sass)")
         .pipe(sass({
             outputStyle: 'compressed'
             })
@@ -33,8 +33,8 @@ gulp.task('styles', function() {
             })
 
 gulp.task('watch', function() {
-    gulp.watch("src/sass/*.+(scss|sass)", gulp.parallel("styles"));
-    gulp.watch("src/*.html").on("change", browserSync.reload);
+    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel("styles"));
+    
 });
 
 gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
