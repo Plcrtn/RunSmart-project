48c0fc5018412c16060c9d11e9f30d3e03e906bf
Dmitry Lisitsyn <polcertaain@gmail.com>
2022-11-23 22:03:39 +0200

    footer

--- gulpfile.js
+++ gulpfile.js
@@ -14,23 +14,25 @@ gulp.task('server', function() {
     gulp.watch("src/*.html").on("change", browserSync.reload);
 });
 
+gulp.task('compass', function() {
+    gulp.src("gulp compasssrc/sass/**/*.+(scss|sass")
+      .pipe(compass({
+        project: src.join(__dirname, 'assets'),
+        css: 'style.min.css',
+        sass: 'style.scss'
+      }))
+      .pipe(gulp.dest('app/assets/temp'));
+  });
+
 gulp.task('styles', function() {
     return gulp.src("src/sass/**/*.+(scss|sass)")
-        .pipe(sass({
-            outputStyle: 'compressed'
-            })
-        .on('error', sass.logError))
-        .pipe(rename({
-            prefix: "",
-            suffix: ".min",
-            }))
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(rename({prefix: "",suffix: ".min",}))
         .pipe(autoprefixer())
-        .pipe(cleanCSS({
-            compatibility: 'ie8'
-            }))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(gulp.dest("src/css"))
         .pipe(browserSync.stream());
-            })
+    })
 
 gulp.task('watch', function() {
     gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel("styles"));
